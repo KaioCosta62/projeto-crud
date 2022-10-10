2954a7bc3eaaf9561bf09aaac46804325064690d
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-10-10 09:50:49 -0300

    criando sistema de criptografia de senhas atraves do bcrypt e organizando a pasta de arquivos utilitarios

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,6 +1,7 @@
 const CostumersModel = require('../models/customers')
+const {crypto} = require('../utils/password')
 
-function add(req,res){
+async function add(req,res){
   const {
     name,
     age,
@@ -8,12 +9,13 @@ function add(req,res){
     password
   } = req.body
 
+  const passwordCrypto = await crypto(password)
 
   const register = new CostumersModel({
     name,
     age,
     email,
-    password
+    password: passwordCrypto
   })
 
   register.save()
